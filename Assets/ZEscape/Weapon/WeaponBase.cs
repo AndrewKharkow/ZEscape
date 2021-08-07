e85a175634bfd201870993445b11a58f9e3dba2a
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-07 22:43:59 +0300

    - Weapon Base

---
+++ Assets/ZEscape/Weapon/WeaponBase.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZEscape.Weapon
+{
+    public abstract class WeaponBase : MonoBehaviour
+    {
+
+    }
+}
