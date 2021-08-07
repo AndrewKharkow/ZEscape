c8512093367244e19e6080c9e5dff3a388e6087b
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-07 20:54:12 +0300

    - Character Base

---
+++ Assets/ZEscape/Character/CharacterBase.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace ZEscape.Character
+{
+    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CapsuleCollider))]
+    [RequireComponent(typeof(NavMeshAgent))]
+
+    public class CharacterBase : MonoBehaviour
+    {
+       
+    }
+}
---
+++ Assets/ZEscape/Character/Zombie/StickmanZombie.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZEscape.Character
+{
+    public class StickmanZombie : CharacterBase
+    {
+
+    }
+}
