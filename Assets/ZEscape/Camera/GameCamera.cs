2b13d20b04e6bf7883c8a90c7673fede2a07508a
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-08 20:35:46 +0300

    - Weapon spawn

--- Assets/ZEscape/Camera/GameCamera.cs
+++ Assets/ZEscape/Camera/GameCamera.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using ZEscape.Settings;
 
 namespace ZEscape.Camera
 {
     public class GameCamera : MonoBehaviour
     {
-       
+        [Inject] private SceneSettings _settings;
+
+        [SerializeField] private Transform _weaponPoint = null;
+
+        private void Start()
+        {
+            Instantiate(_settings.GunPrefab, _weaponPoint);
+        }
     }
 }
