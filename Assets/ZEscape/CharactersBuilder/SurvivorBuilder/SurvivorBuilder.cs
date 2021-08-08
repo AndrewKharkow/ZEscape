057bb80ec36f93abd0c77b4757fb049ce8aa3e1b
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-08 20:36:30 +0300

    - SpawnPlayers

--- Assets/ZEscape/CharactersBuilder/SurvivorBuilder/SurvivorBuilder.cs
+++ Assets/ZEscape/CharactersBuilder/SurvivorBuilder/SurvivorBuilder.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
+using ZEscape.Character;
+using ZEscape.Settings;
 
 namespace ZEscape.CharacterBuilder
 {
     public class SurvivorBuilder : MonoBehaviour
     {
+        [Inject] private SceneSettings _settings;
+        [SerializeField] private int _survivorsCount;
+        [SerializeField] private List<CharacterBase> _survivors;
 
+        private Transform Transform => transform;
+
+        private void Start()
+        {
+            for(int i = 0; i < _survivorsCount; i++)
+            {
+                CharacterBase survivor;
+                survivor = Instantiate(_settings.SurvivorPrefab, transform);
+                _survivors.Add(survivor); 
+            }
+        }
     }
 }
