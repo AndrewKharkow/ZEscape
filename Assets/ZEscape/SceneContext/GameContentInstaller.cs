00c3933bacb6a4860793f0f2dc884fd38cf7fd9e
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-08 20:19:39 +0300

    - Game Installer

--- Assets/ZEscape/SceneContext/GameContentInstaller.cs
+++ Assets/ZEscape/SceneContext/GameContentInstaller.cs
@@ -1,9 +1,17 @@
+using Game.Gui;
 using UnityEngine;
 using Zenject;
+using ZEscape.Camera;
+using ZEscape.CharacterBuilder;
+using ZEscape.Settings;
 
 public class GameContentInstaller : MonoInstaller
 {
     public override void InstallBindings()
     {
+        Container.Bind<SceneSettings>().FromComponentInHierarchy(true).AsSingle();
+        Container.Bind<GameCamera>().FromComponentInHierarchy(true).AsSingle();
+        Container.Bind<GuiHandler>().FromComponentInHierarchy(true).AsSingle();
+        Container.Bind<SurvivorBuilder>().FromComponentInHierarchy(true).AsSingle();
     }
 }
