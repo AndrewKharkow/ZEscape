a67d4624bfdd8242db6ef7738a918432aa06579a
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-09 02:25:58 +0300

    - Gun base behaviour

--- Assets/ZEscape/Weapon/WeaponBase.cs
+++ Assets/ZEscape/Weapon/WeaponBase.cs
@@ -6,6 +6,24 @@ namespace ZEscape.Weapon
 {
     public abstract class WeaponBase : MonoBehaviour
     {
+        [SerializeField] private GameObject _muzzleFlash;
 
+        private void Update()
+        {
+            if(Input.touchCount == 1)
+            {
+                Touch touch = Input.GetTouch(0);
+
+                switch (touch.phase)
+                {
+                    case TouchPhase.Began:
+                        _muzzleFlash.SetActive(true);
+                        break;
+                    case TouchPhase.Ended:
+                        _muzzleFlash.SetActive(false);
+                        break;
+                }
+            }
+        }
     }
 }
