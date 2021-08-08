b0a87b27d626e923de438823273c93dbee68fddd
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-08 23:42:53 +0300

    - Scene manager

--- Assets/ZEscape/GameSceneManager.cs
+++ Assets/ZEscape/GameSceneManager.cs
@@ -1,17 +1,20 @@
 using Game.Gui;
 using System;
+using UnityEngine;
 using Zenject;
 using ZEscape.Camera;
+using ZEscape.CharacterBuilder;
 using ZEscape.Settings;
 
-namespace Game
+namespace ZEscape
 {
     public class GameSceneManager : ITickable, IInitializable, IDisposable
     {
 
         [Inject] private GameCamera _camera;
         [Inject] private SceneSettings _settings;
-        [Inject] private GuiHandler _gui; 
+        [Inject] private GuiHandler _gui;
+        [Inject] private SurvivorBuilder _survivorBuilder;
 
         private enum GameState
         {
@@ -24,12 +27,15 @@ namespace Game
         // private GameState CurrentGameState = GameState.Start;
         public void Initialize()
         {
-            
         }
 
         public void Tick()
         {
-
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("RUN");
+                _survivorBuilder.RunToEscape();
+            }
         }
 
         public void Dispose()
