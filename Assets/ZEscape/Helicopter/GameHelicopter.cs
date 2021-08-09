2dcfa183707271e5cea69c219fe46c6cd2022bca
AndrewKharkow <andrewbullet1996@gmail.com>
2021-08-09 04:13:58 +0300

    - Fly Helicopter

--- Assets/ZEscape/Helicopter/GameHelicopter.cs
+++ Assets/ZEscape/Helicopter/GameHelicopter.cs
@@ -2,36 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
-using ZEscape.Character;
-using ZEscape.CharacterBuilder;
+using Game.Character;
+using Game.CharacterBuilder;
 
 namespace ZEscape.Helicopter
 {
     [RequireComponent(typeof(BoxCollider))]
+
     public class GameHelicopter : MonoBehaviour
     {
         [Inject] private SurvivorBuilder _survivorBuilder;
+        private Animator Animator => GetComponent<Animator>();
         public int SurvivorsCount;
+
         private void Awake()
         {
             GetComponent<BoxCollider>().isTrigger = true;
-            _survivorBuilder.CharactersAreOver += _survivorBuilder_CharactersAreOver;
-        }
-
-        private void _survivorBuilder_CharactersAreOver()
-        {
-            Debug.Log("CharactersAreOver");
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if(other.tag == "Survivor")
+            if (other.tag == "Survivor")
             {
                 CharacterBase character = other.GetComponent<CharacterBase>();
                 _survivorBuilder.RemoveCharacterFromList(character);
                 SurvivorsCount++;
                 Destroy(other.gameObject);
             }
+        }
+
+        public void Fly()
+        {
+            Animator.SetTrigger("Fly");
         }
     }
 }
